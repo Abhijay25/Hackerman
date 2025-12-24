a99832badc85d8b8a995a2fe47d8fc5c4accb879
Abhijay <163997617+Abhijay25@users.noreply.github.com>
2025-12-24 18:33:12 +0800

    Setup for Hackerman

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Hackerman Starter Kit";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          # --- CONNECTIVITY ---
+          netcat-gnu      # Connects to challenge servers.
+          nmap            # Simple network scanning.
+          wget            # Downloading challenge files.
+
+          # --- FORENSICS ---
+          file            # Tells you if a file is a JPEG, ELF, Text, etc.
+          binutils        # Includes 'strings' (Finds text in binary files).
+          exiftool        # Reads metadata (GPS, Author) from images.
+          binwalk         # Checks if one file is hidden inside another.
+          unzip           # Extracting archives.
+
+          # --- SCRIPTING & CRYPTO ---
+          python3
+          gcc
+        ];
+      };
+    };
+}
